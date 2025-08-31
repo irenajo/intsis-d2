27274413c8c17cf9443ba7ec0498ea75064f5c5d
Luka Gojic <gojicluka@gmail.com>
2025-08-31 18:24:13 +0200

    minimaxAB done

--- TEST.py
+++ TEST.py
@@ -1,40 +1,21 @@
-max_depth = 3
-
-# start with player_max == TRUE!
-# todo what is start depth?? ajde recimo da je 1 za sad...
-def minimax(player_max : bool, curr_depth : int, state):
-    if curr_depth == max_depth:
-        # reached end - get score
-        return state.get_score(state.get_on_move_char())
-
-    # have not reached end
-    possible_actions = state.get_legal_actions()
-
-    if player_max == True:
-        score = float('-inf')
-        for my_action in possible_actions:
-            possible_state = state.generate_successor_state(my_action)
-            score = max(score, minimax(False, curr_depth + 1, possible_state))
+def minimax_alpha_beta(node, player, alpha, beta):
+    if is_terminal_node(node):
+        return node_evaluation(node)
+    
+    if player == Player.MAX:
+        score = -math.inf
+        for succ in node.successors():
+            score = max(score, minimax_alpha_beta(succ, Player.MIN, alpha, beta))
+            alpha = max(alpha, score)
+            if alpha >= beta: 
+                break # alpha-cut
         return score
     else:
-        score = float('inf')
-    for my_action in possible_actions:
-        possible_state = state.generate_successor_state(my_action)
-        score = min(score, minimax(True, curr_depth + 1, possible_state))
+        score = +math.inf
+        for succ in node.successors():
+            score = min(score, minimax_alpha_beta(succ, Player.MAX, alpha, beta))
+            beta = min(beta, score)
+            if alpha >= beta:
+                break # beta-cut
     return score
 
-
-def get_chosen_action(state, max_depth):
-    time.sleep(0.5)
-    possible_actions = state.get_legal_actions()
-    score = float('-inf')
-    next_action = [] # todo what is a valid no-action?
-    # starting as a MAX player, this is first iteration because we want to return the action associated with it
-    for action in possible_actions:
-        possible_state = state.generate_successor_state(action)
-        possible_score = self.minimax(False, 1, possible_state, max_depth)
-        if possible_score > score:
-            score = possible_score
-            next_action = action
-
-    return next_action
--- agents.py
+++ agents.py
@@ -81,5 +81,62 @@ class MinimaxAgent(Agent):
         return next_action
 
 
+class MinimaxABAgent(Agent):
+
+    def minimaxAB(self, player_max, curr_depth, state, max_depth, root_player, alpha, beta):
+        if state.is_goal_state() or curr_depth == max_depth:
+            # reached end - get score
+            if root_player == 'A':
+                return (state.get_score('A') - state.get_score('B'))
+            else:
+                return (state.get_score('B') - state.get_score('A'))
+
+        # have not reached end
+        possible_actions = state.get_legal_actions()
+
+        if player_max == True:
+            score = float('-inf')
+            for my_action in possible_actions:
+                possible_state = state.generate_successor_state(my_action)
+                score = max(score, MinimaxABAgent.minimaxAB(self,False, curr_depth + 1, possible_state, max_depth, root_player, alpha, beta))
+                alpha = max(alpha, score)
+                if alpha >= beta: 
+                    break 
+            return score
+        else:
+            score = float('inf')
+            for my_action in possible_actions:
+                possible_state = state.generate_successor_state(my_action)
+                score = min(score, MinimaxABAgent.minimaxAB(self,True, curr_depth + 1, possible_state, max_depth, root_player,alpha,beta))
+                beta = min(beta, score)
+                if alpha >= beta:
+                    break 
+        return score
+
+    def get_chosen_action(self, state, max_depth):
+        time.sleep(0.5)
+        possible_actions = state.get_legal_actions()
+
+        if len(possible_actions) == 0:
+            return None
+
+        alpha = float('-inf')
+        beta = float('inf')
+        best_score = float('-inf')
+        next_action = possible_actions[0]
+        root_player = state.get_on_move_chr()
+        # starting as a MAX player, this is first iteration because we want to return the action associated with it
+        for action in possible_actions:
+            possible_state = state.generate_successor_state(action)
+            curr_score = MinimaxABAgent.minimaxAB(self, False, 1, possible_state, max_depth, root_player, alpha, beta)
+            if curr_score > best_score:
+                best_score = curr_score
+                next_action = action
+            alpha = max(alpha, best_score)
+            if alpha >= beta:
+                break # beta-cut
+        return next_action
+
+
 # start with player_max == TRUE!
 # todo what is start depth?? ajde recimo da je 1 za sad...
